20910101fde3c9bc1864d628f28252dabfc9647f
Benjamin Baer <benjamin.baer@math.uzh.ch>
2024-06-29 18:42:57 +0200

    made animation trigger

--- assets/characters/pawn.gd
+++ assets/characters/pawn.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
 @export var path_finder : Pathfinder
 @export var ground_grid  : TileMap
 var current_id_path : Array[Vector2i]
+var current_anim = "idle"
+var is_moving = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,14 +22,32 @@ func _input(event):
 	print(id_path)
 	if(id_path.is_empty()) == false:
 		current_id_path = id_path
+		
+func _process(delta):
+	if(is_moving):
+		update_animation("walk")
+	else:
+		animation_player.stop()
+		update_animation("idle")
 
 func _physics_process(delta):
 	if current_id_path.is_empty():
+		is_moving = false
 		return
 	
 	var target_position = ground_grid.map_to_local(current_id_path.front())
+	if(target_position.x < global_position.x):
+		scale = Vector2(-1.0,1.0)
+	else:
+		scale = Vector2(1.0,1.0)
 	
 	global_position = global_position.move_toward(target_position, 1)
-	
+	is_moving = true
 	if global_position == target_position:
 		current_id_path.pop_front()
+
+func update_animation(name):
+	if (current_anim == name):
+		return
+	animation_player.play(name)
+
