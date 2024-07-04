3b518a27cec1caeaf46485f6220b66fc2b05258f
Benjamin Baer <benjamin.baer@math.uzh.ch>
2024-07-04 13:00:08 +0200

    harvest target

--- assets/items/plants/ClickTarget.gd
+++ assets/items/plants/ClickTarget.gd
@@ -1,7 +1,10 @@
-extends Area2D
+extends Node
+
+@export var sprite : Sprite2D
 
 func _input(event):
 	if event.is_action_pressed("act"):
-		get_parent().on_click()
+		if sprite.is_pixel_opaque(sprite.get_local_mouse_position()):
+			get_parent().on_click()
 		
 		
